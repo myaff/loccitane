59e3dc0a68a65064e5931aaf95ff7f64b79ecdce
myaff <grabka74@gmail.com>
2017-09-19 00:17:01 +0500

    podkaminskaya fin bleat

--- podkaminskaya.php
+++ podkaminskaya.php
@@ -2,57 +2,57 @@
 <div class="page page--ep">
   <div class="container">
     <div class="page__content girl girl--big">
-      <div class="page__heading girl__name tp-text--center mb-5">
+      <div class="ep-1 page__heading girl__name tp-text--center mb-4">
         <div class="girl__first-name">Елена</div>
         <div class="girl__last-name">Подкаминская</div>
       </div>
-      <div class="l-block--left img-wrapper ep-1 mt-7 a-parallax-box">
-        <img class="l-block--right a-parallax-middle" src="build/img/podkaminskaya/podkaminskaya-1.jpg" alt="Елена Подкаминская"/>
-      </div>
-      <div class="text-block l-block--left ep-2 a-scroll-box">
-        <div class="tp-body--main a-fade-slide a-delay-1">
-          <p><b>Когда есть немного времени,</b><br/>
-            для меня счастье сходить к косметологу на процедуру jet peel и сделать увлажняющую маску. А если еще и выспаться перед этим - это вообще невероятная роскошь. И, конечно, очень важна команда, с который мы понимаем друг друга с полуслова - мне необходимо быть на одной волне со стилистом и визажистом, тогда и результат не разочаровывает, и ощущение праздника наступает еще до первого шага по красной дорожке. </p>
-        </div>
-      </div>
-      <div class="l-block--right ep-3">
+      <div class="ep-2">
         <div class="img-wrapper a-parallax-box">
-          <img class="l-block--left a-parallax-middle" src="build/img/podkaminskaya/podkaminskaya-2.jpg" alt="Елена Подкаминская" data-parallax-from="0" data-parallax-to="10"/>
+          <img class="l-block--right a-parallax-middle" src="build/img/podkaminskaya/podkaminskaya-1.jpg" alt="Елена Подкаминская"/>
         </div>
-        <div class="a-scroll-box">
+        <div class="text-block mt-3 a-scroll-box">
+          <div class="tp-body--main a-fade-slide a-delay-1">
+            <p><b>Когда есть немного времени,</b><br/>
+              для меня счастье сходить к косметологу на процедуру jet peel и сделать увлажняющую маску. А если еще и выспаться перед этим - это вообще невероятная роскошь. И, конечно, очень важна команда, с который мы понимаем друг друга с полуслова - мне необходимо быть на одной волне со стилистом и визажистом, тогда и результат не разочаровывает, и ощущение праздника наступает еще до первого шага по красной дорожке. </p>
+          </div>
+        </div>
+      </div>
+      <div class="ep-3 pt-8">
+        <div class="img-wrapper a-parallax-box">
+          <img class="a-parallax-middle" src="build/img/podkaminskaya/podkaminskaya-2.jpg" alt="Елена Подкаминская" data-parallax-from="0" data-parallax-to="10"/>
+        </div>
+        <div class="blockquote-wrapper a-scroll-box">
           <blockquote class="mt-8 a-fade-slide a-delay-2">Обожаю хорошую баню. Нет ничего лучше старой доброй парной - для тела и духа, каждый раз будто рождаешься заново</blockquote>
         </div>
-      </div>
-      <div class="text-block l-block--left ep-4 ml-8 mt-5 a-scroll-box">
-        <div class="tp-body--main mb-5 a-fade-slide a-ordered">
-          <p><b>Я очень люблю «Карите»</b> - легкий крем для тела, он увлажняет кожу за считанные минуты. Обязательно ухаживаю за кожей вокруг глаз с помощью «Божественного бальзама». А бальзам для губ и крем для рук L’Occitane - уже несколько лет «прописаны» в моей ежедневной косметичке</p>
-        </div>
-        <div class="tp-body--main a-fade-slide a-ordered">
-          <p><b>Всегда внимательно отношусь к тому, что ем</b>, но иногда могу позволить себе запретные радости, вроде пиццы, если очень хочется! Во время беременности неожиданно для себя завтракала кофе и бутербродами с сыром и маслом – никогда бы не подумала, что это про меня.</p>
-        </div>
-        <button class="btn btn--big btn--dark js-modal mt-5 a-fade-slide a-ordered" data-target="#competition">конкурс</button>
-      </div>
-      <div class="l-block--left img-wrapper ep-5 a-parallax-box">
-        <img class="l-block--right a-parallax-front" src="build/img/podkaminskaya/podkaminskaya-3.jpg" alt="Елена Подкаминская" data-parallax-from="-5" data-parallax-to="2"/>
-      </div>
-      <div class="text-block l-block--left ep-6 ml-8 a-scroll-box">
-        <div class="tp-body--main a-fade-slide">
-          <p><b>Что помогло мне принять и полюбить себя?</b> Мне кажется, все идет из детства. Если ты любим в семье, если твои проблемы решают и участвуют в твоей жизни, ты окружен заботой, теплом и вниманием - тогда и зарождается любовь к себе. Я всегда знала, что за мной стоят мои близкие.</p>
+        <div class="text-block mt-8 pt-3 ml-8 a-scroll-box">
+          <div class="tp-body--main mb-5 a-fade-slide a-ordered">
+            <p><b>Я очень люблю «Карите»</b> - легкий крем для тела, он увлажняет кожу за считанные минуты. Обязательно ухаживаю за кожей вокруг глаз с помощью «Божественного бальзама». А бальзам для губ и крем для рук L’Occitane - уже несколько лет «прописаны» в моей ежедневной косметичке</p>
+          </div>
+          <div class="tp-body--main a-fade-slide a-ordered">
+            <p><b>Всегда внимательно отношусь к тому, что ем</b>, но иногда могу позволить себе запретные радости, вроде пиццы, если очень хочется! Во время беременности неожиданно для себя завтракала кофе и бутербродами с сыром и маслом – никогда бы не подумала, что это про меня.</p>
+          </div>
+          <button class="btn btn--big btn--dark js-modal mt-8 a-fade-slide a-ordered" data-target="#competition">конкурс</button>
         </div>
       </div>
-      <div class="l-block--left ep-7 mt-4 mb-8 ml-8 a-scroll-box">
-        <img class="a-fade-slide a-ordered" src="build/img/podkaminskaya/podkaminskaya-prod.png" alt="Елена Подкаминская"/>
-        <div class="text-block ml-4 a-scroll-box">
-          <div class="tp-heading--medium mb-3 a-fade-slide a-ordered">Божественный бальзам <br/>для кожи вокруг глаз</div>
+      <div class="ep-4 py-5">
+        <div class="img-wrapper mt-8 a-parallax-box">
+          <img class="l-block--right a-parallax-front" src="build/img/podkaminskaya/podkaminskaya-3.jpg" alt="Елена Подкаминская" data-parallax-from="-5" data-parallax-to="2"/>
+        </div>
+        <div class="text-block a-scroll-box">
+          <div class="tp-body--main a-fade-slide">
+            <p><b>Что помогло мне принять и полюбить себя?</b> Мне кажется, все идет из детства. Если ты любим в семье, если твои проблемы решают и участвуют в твоей жизни, ты окружен заботой, теплом и вниманием - тогда и зарождается любовь к себе. Я всегда знала, что за мной стоят мои близкие.</p>
+          </div>
+          <img class="mt-5 a-fade-slide a-ordered" src="build/img/podkaminskaya/podkaminskaya-prod.png" alt="Елена Подкаминская"/>
+          <div class="tp-heading--medium mb-1 a-fade-slide a-ordered">Божественный бальзам <br/>для кожи вокруг глаз</div>
           <div class="tp-body--main a-fade-slide a-ordered">
             <p>Бальзам прекрасно разглаживает морщинки и повышает упругость кожи вокруг глаз, снимает отечность и раздражение. В его состав входят воск гречихи и экстракт ясеня, которые помогают избавиться от темных кругов под глазами. А цветочная вода розы успокаивает кожу и придает взгляду свежесть и сияние. Невероятно нежную текстуру средства особенно оценят женщины с сухой кожей.</p>
           </div>
           <button class="btn mt-5 a-fade-slide a-ordered">Купить</button>
         </div>
       </div>
-      <div class="l-block--center mt-8 ep-8 a-scroll-box">
+      <div class="ep-5 l-block--center mt-8 a-scroll-box">
         <blockquote class="l-block--center a-fade-slide">Точное понимание того, что нахожусь в правильном месте, в правильной точке, придает мне уверенности и приближает к тому, что называют</blockquote>
-        <button class="btn btn--big btn--dark btn--center js-modal mt-5 a-fade-slide a-delay-1" data-target="#competition">конкурс</button>
+        <button class="btn btn--big btn--dark btn--center js-modal a-fade-slide a-delay-1" data-target="#competition">конкурс</button>
       </div>
     </div>
     <div class="social__wrapper my-8">
