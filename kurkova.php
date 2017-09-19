a0cb3b4f831f933a31fcda209041cce7731c2ce4
myaff <grabka74@gmail.com>
2017-09-19 05:04:41 +0500

    kurkova fin

--- kurkova.php
+++ kurkova.php
@@ -2,32 +2,32 @@
 <div class="page page--rk">
   <div class="container">
     <div class="page__content girl girl--big">
-      <div class="l-block--left rk-1">
-        <div class="page__heading girl__name tp-text--center mb-3">
+      <div class="rk-1 pt-7 pb-8">
+        <div class="page__heading girl__name tp-text--center">
           <div class="girl__first-name">Равшана</div>
           <div class="girl__last-name">Куркова</div>
         </div>
         <div class="img-wrapper a-parallax-box">
           <img class="l-block--right a-parallax-front" src="build/img/kurkova/kurkova-1.jpg" alt="Равшана Куркова" data-parallax-from="-5" data-parallax-to="2"/>
         </div>
-      </div>
-      <div class="l-block--right rk-2 a-scroll-box">
-        <div class="text-block ml-7 a-fade-slide a-ordered">
-          <div class="tp-body--main">
-            <p>За лицом ухаживаю дома - нанесу маску и могу одновременно приседать со штангой и повторять текст роли, например. А вот для массажа нужно выделять специальное время. И когда его удается найти - это невероятное удовольствие, периодически себя им балую.</p>
+        <div class="text-block-wrapper a-scroll-box pb-8 mt-2 mb-7">
+          <div class="text-block ml-8 a-fade-slide a-ordered">
+            <div class="tp-body--main">
+              <p>За лицом ухаживаю дома - нанесу маску и могу одновременно приседать со штангой и повторять текст роли, например. А вот для массажа нужно выделять специальное время. И когда его удается найти - это невероятное удовольствие, периодически себя им балую.</p>
+            </div>
+          </div>
+          <div class="tp-heading--huge mt-4 mb-7 a-fade-slide a-ordered">Я за здоровый образ жизни</div>
+          <div class="text-block ml-8 a-fade-slide a-ordered">
+            <div class="tp-body--main">
+              <p>Замечаю, что кожа в последнее время стала склонна к пигментации. Поэтому я теперь пользуюсь защитным кремом Божественный Иммортель SPF20. К такой «тяжелой артиллерии», как инъекции, я не прибегаю, но вот антивозрастные флюиды начинаю понемногу вводить в свой обиход.</p>
+            </div>
           </div>
         </div>
-        <div class="tp-heading--huge mt-3 mb-7 a-fade-slide a-ordered">Я за здоровый образ жизни</div>
-        <div class="text-block ml-7 a-fade-slide a-ordered">
-          <div class="tp-body--main">
-            <p>Замечаю, что кожа в последнее время стала склонна к пигментации. Поэтому я теперь пользуюсь защитным кремом Божественный Иммортель SPF20. К такой «тяжелой артиллерии», как инъекции, я не прибегаю, но вот антивозрастные флюиды начинаю понемногу вводить в свой обиход.</p>
-          </div>
+      </div>
+      <div class="rk-2 pt-6 pl-8">
+        <div class="img-wrapper a-parallax-box">
+          <img class="l-block--left a-parallax-front" src="build/img/kurkova/kurkova-2.jpg" alt="Равшана Куркова" data-parallax-from="-10"/>
         </div>
-      </div>
-      <div class="l-block--right img-wrapper rk-3 a-parallax-box">
-        <img class="l-block--left a-parallax-front" src="build/img/kurkova/kurkova-2.jpg" alt="Равшана Куркова" data-parallax-from="-10"/>
-      </div>
-      <div class="l-block--left rk-4 mt-5 mb-3 ml-8 ml-md-6">
         <div class="text-block a-scroll-box">
           <div class="tp-body--main a-fade-slide a-ordered">
             <p><b>Когда надо срочно хорошо выглядеть</b> – накладываю увлажняющую маску на лицо, а через 10–15 минут снимаю, распределяю остатки средства на лицо и шею и добавляю немного нежных кремовых румян. Затем – чуть-чуть корректора от синяков и бальзам для губ. Эффект – сияющая здоровая кожа.</p>
@@ -35,34 +35,33 @@
           <button class="btn btn--big btn--dark js-modal mt-6 a-fade-slide a-ordered" data-target="#competition">конкурс</button>
         </div>
       </div>
-      <div class="l-block--center rk-5 mt-2 mb-5 a-scroll-box">
+      <div class="rk-3 l-block--center mb-5 a-scroll-box">
         <blockquote class="l-block--center a-fade-slide">Я почти не пользуюсь декоративной косметикой в обычной жизни, но несмотря ни на что, каждый вечер очищаю лицо тоником. Ложиться спать, не очистив кожу лица – для меня табу</blockquote>
       </div>
-      <div class="l-block--left img-wrapper img-wrapper--big rk-6 a-parallax-box">
-        <img class="l-block--right mr-2 a-parallax-front" src="build/img/kurkova/kurkova-3.jpg" alt="Равшана Куркова" data-parallax-from="-4" data-parallax-to="3"/>
-      </div>
-      <div class="l-block--left rk-7 a-scroll-box">
-        <div class="text-block a-fade-slide">
-          <div class="tp-body--main">
-            <p><b>Первый бьюти-эксперт в моей жизни – это бабушка.</b> Она всегда была очень ухоженной: идеальная причёска, маникюр. И в 90 лет практически не было морщин. Бабушка жила в Ташкенте, была известным врачом и верила: все продукты, ценные для организма, полезны и нашей коже. Она наносила на лицо и ряженку, и клубнику, и я так тоже делала в юности. Сейчас у нас, конечно, большой выбор косметики в баночках.</p>
+      <div class="rk-4 mb-8">
+        <div class="img-wrapper img-wrapper--big a-parallax-box">
+          <img class="l-block--right mr-2 a-parallax-front" src="build/img/kurkova/kurkova-3.jpg" alt="Равшана Куркова" data-parallax-from="-4" data-parallax-to="3"/>
+        </div>
+        <div class="text-block-wrapper a-scroll-box">
+          <div class="text-block mb-8 a-fade-slide a-ordered">
+            <div class="tp-body--main">
+              <p><b>Первый бьюти-эксперт в моей жизни – это бабушка.</b> Она всегда была очень ухоженной: идеальная причёска, маникюр. И в 90 лет практически не было морщин. Бабушка жила в Ташкенте, была известным врачом и верила: все продукты, ценные для организма, полезны и нашей коже. Она наносила на лицо и ряженку, и клубнику, и я так тоже делала в юности. Сейчас у нас, конечно, большой выбор косметики в баночках.</p>
+            </div>
+          </div>
+          <img class="a-fade-slide a-ordered" src="build/img/kurkova/kurkova-4.png" alt="Равшана Куркова"/>
+          <div class="text-block a-scroll-box">
+            <div class="tp-heading--medium mt-3 mb-1 a-fade-slide a-ordered">Защитный крем Божественный Иммортель SPF20 </div>
+            <div class="tp-body--main a-fade-slide a-ordered">
+              <p>Благодаря легкой текстуре крем мгновенно впитывается, позволяя коже дышать. Особые достоинства средства – в его разнонаправленном действии.  Крем защищает от агрессивного воздействия окружающей среды и УФ-лучей, и в то же время борется с первыми признаками старения кожи. При регулярном применении морщины разглаживаются, кожа становится более эластичной и сияющей.</p>
+            </div>
+            <button class="btn mt-3 mb-5 a-fade-slide a-ordered">Купить</button>
           </div>
         </div>
-      </div>
-      <div class="l-block--left rk-8 my-7 ml-5 a-scroll-box">
-        <img class="a-fade-slide" src="build/img/kurkova/kurkova-4.png" alt="Равшана Куркова"/>
-        <div class="text-block ml-4 a-scroll-box">
-          <div class="tp-heading--medium mt-4 mb-2 a-fade-slide a-ordered">Защитный крем Божественный Иммортель SPF20 </div>
-          <div class="tp-body--main a-fade-slide a-ordered">
-            <p>Благодаря легкой текстуре крем мгновенно впитывается, позволяя коже дышать. Особые достоинства средства – в его разнонаправленном действии.  Крем защищает от агрессивного воздействия окружающей среды и УФ-лучей, и в то же время борется с первыми признаками старения кожи. При регулярном применении морщины разглаживаются, кожа становится более эластичной и сияющей.</p>
-          </div>
-          <button class="btn mt-3 mb-7 a-fade-slide a-ordered">Купить</button>
+        <div class="blockquote-wrapper ml-4 a-scroll-box">
+          <blockquote class="a-fade-slide a-ordered">Знаю прекрасный способ запустить метаболизм: я добавляю в тыквенное масло сок лимона, немного соли и макаю туда кусочек черного хлеба – не изменяю этому ритуалу уже много лет</blockquote>
+          <button class="btn btn--big btn--dark btn--center js-modal mt-8 a-fade-slide a-ordered" data-target="#competition">конкурс</button>
         </div>
       </div>
-      <div class="l-block--left rk-9 ml-4 a-scroll-box">
-        <blockquote class="a-fade-slide a-ordered">Знаю прекрасный способ запустить метаболизм: я добавляю в тыквенное масло сок лимона, немного соли и макаю туда кусочек черного хлеба – не изменяю этому ритуалу уже много лет</blockquote>
-        <button class="btn btn--big btn--dark btn--center js-modal mt-8 a-fade-slide a-ordered" data-target="#competition">конкурс</button>
-      </div>
-      <div class="l-block--center"></div>
     </div>
     <div class="social__wrapper mb-8">
       <?php include('parts/social-inline.php'); ?>
